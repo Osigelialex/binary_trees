20e6773b51dca4776e6c7cf356d9f5be4b169dcd
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-02-28 16:29:55 +0300

    Fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,11 +8,22 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	else
-		return (0);
+	if (node == null)       
+        return false;
+    if (node.left == null && node.right == null)      
+        return true; 
+    isLeaf(node.left); 
+    isLeaf(node.right);      
 }
+
+return (isLeaf(node.left, data) || isLeaf(node.right, data));
+
+
+boolean isLeaf(BTNode node, int data) {
+    if (node == null)
+        return false;    
+    if (node.right == null && node.left == null)
+        return true;
+    return false; 
+}
+
