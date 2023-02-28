62d49e15f1c45bebb53199ac4ea1b81999171071
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-02-28 16:32:37 +0300

    Done :heavy_check_mark:

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,24 +6,8 @@
  * @node: pointer to the node to check
  * Return: 0 or 1
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == null)       
-        return false;
-    if (node.left == null && node.right == null)      
-        return true; 
-    isLeaf(node.left); 
-    isLeaf(node.right);      
+	return ((!node || node->left || node->right) ? 0 : 1);
 }
-
-return (isLeaf(node.left, data) || isLeaf(node.right, data));
-
-
-boolean isLeaf(BTNode node, int data) {
-    if (node == null)
-        return false;    
-    if (node.right == null && node.left == null)
-        return true;
-    return false; 
-}
-
