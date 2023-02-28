4c3ba26cf9a74502035dcf6b6025ce4434350cbd
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-02-28 17:27:11 +0300

    Done :heavy_check_mark:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,8 +10,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node =  malloc(sizeof(binary_tree_t));
-
+	binary_tree_t *new_node;
+	
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return NULL;
 
@@ -20,5 +21,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->n = value;
 
-	return new_node;
+	return (new_node);
 }
