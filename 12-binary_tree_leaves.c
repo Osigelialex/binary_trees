0ed516ce2882b90d61373e9fd0b591a1b8d41d01
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-03-01 00:32:22 +0300

    Update :heavy_check_mate:

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,10 +9,10 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	if (!tree->left && !tree->right)
+	if (!tree = NULL->left && !tree->right)
 		return (1);
 
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
