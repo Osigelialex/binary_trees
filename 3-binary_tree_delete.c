c7c20d5450364dc61773fedc149fe6cef828f3f9
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-03-01 16:49:37 +0300

    Fixed :hammer_and_wrench:

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,8 +6,8 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-    return;
+    if (tree == NULL)
+        return;
     binary_tree_delete(tree->right);
     binary_tree_delete(tree->left);
     free(tree);
