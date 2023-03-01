ded5a78737eadbe871634e9df1593c7a2776b598
Osigelialex <osigelialex@gmail.com>
2023-03-01 10:43:59 -0800

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,12 +7,12 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (!tree)
 		return (0);
 
-	int left, right;
-
-	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	left = tree->left ? (1 + binary_tree_balance(tree->left)) : 0;
+	right = tree->right ? (1 + binary_tree_balance(tree->right)) : 0;
 	return (left - right);
 }
