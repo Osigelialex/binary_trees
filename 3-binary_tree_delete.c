758a19cc42e83a87cc498a90611f77ec7a3a3d4a
3SSI3 <starsm004@gmail.com>
2023-03-02 13:20:46 +0300

    Fix:hammer_and_wrench:

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,13 +2,14 @@
 
 /**
  * binary_tree_delete - deletes an entire binary tree
+ *
  * @tree: tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    binary_tree_delete(tree->right);
-    binary_tree_delete(tree->left);
-    free(tree);
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
