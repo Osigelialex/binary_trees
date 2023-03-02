a5adfab5e667c4523199178dc2006caa7b915501
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2023-03-02 13:09:30 +0300

    Fix :hammer_and_wrench:

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,21 +11,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-	if (parent == NULL)
+	if (new_node == NULL || parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = parent->right;
-	parent->right = new_node;
-	if (new_node->right)
-		new_node->right->parent = new_node;
-
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right = new_node;
+	}
+	else
+	{
+		parent->right = new_node;
+	}
 	return (new_node);
 }
